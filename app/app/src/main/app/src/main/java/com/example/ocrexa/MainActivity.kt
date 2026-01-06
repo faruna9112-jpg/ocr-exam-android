717fc0c0ac5c92559e88ac644d2370188aca628e
faruna9112-jpg <faruna9112@gmail.com>
2026-01-07 00:11:44 +0200

    MainActivity.kt
    package com.example.ocrexa
    
    import android.content.Intent
    import android.os.Bundle
    import android.widget.Button
    import androidx.appcompat.app.AppCompatActivity
    
    class MainActivity : AppCompatActivity() {
    
        override fun onCreate(savedInstanceState: Bundle?) {
            super.onCreate(savedInstanceState)
            setContentView(R.layout.activity_main)
    
            val btn = findViewById<Button>(R.id.openFormBtn)
            btn.setOnClickListener {
                startActivity(
                    Intent(this, FormActivity::class.java)
                )
            }
        }
    }

--- app/app/src/main/app/src/main/java/com/example/ocrexa/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/example/ocrexa/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ocrexa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.openFormBtn)
+        btn.setOnClickListener {
+            startActivity(
+                Intent(this, FormActivity::class.java)
+            )
+        }
     }
 }
