c2b67a41864299ed023cf2e6e00e60c33f72f475
faruna9112-jpg <faruna9112@gmail.com>
2026-01-07 00:07:00 +0200

    FormActivity.kt
    package com.example.ocrexa
    
    import android.os.Bundle
    import android.webkit.WebView
    import android.webkit.WebViewClient
    import androidx.appcompat.app.AppCompatActivity
    
    class FormActivity : AppCompatActivity() {
    
        override fun onCreate(savedInstanceState: Bundle?) {
            super.onCreate(savedInstanceState)
    
            val webView = WebView(this)
            webView.settings.javaScriptEnabled = true
            webView.webViewClient = WebViewClient()
    
            setContentView(webView)
    
            webView.loadUrl(
                "https://docs.google.com/forms/d/e/ВАШ_ID_ФОРМИ/viewform"
            )
        }
    }

---
+++ app/src/main/java/com/example/ocrexa/FormActivity.kt
@@ -0,0 +1,23 @@
+package com.example.ocrexa
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        setContentView(webView)
+
+        webView.loadUrl(
+            "https://docs.google.com/forms/d/e/ВАШ_ID_ФОРМИ/viewform"
+        )
+    }
+}
