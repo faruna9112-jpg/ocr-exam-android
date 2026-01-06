d635cb1a2fc27d812a1c7f4eea179b0ac1c0bcc1
faruna9112-jpg <faruna9112@gmail.com>
2026-01-06 23:48:49 +0200

    app/src/main/java/com/example/ocrexa/MainActivity.kt
    package com.example.ocrexa
    
    import android.os.Bundle
    import androidx.appcompat.app.AppCompatActivity
    
    class MainActivity : AppCompatActivity() {
        override fun onCreate(savedInstanceState: Bundle?) {
            super.onCreate(savedInstanceState)
        }
    }

---
+++ app/app/src/main/app/src/main/java/com/example/ocrexa/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.ocrexa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
